6d6c87344bd36b77263ce0194f8d595fba77c1ba
Bas Rustenburg <bas-rustenburg@users.noreply.github.com>
2014-09-20 19:53:45 -0400

    Create TPYoga_PenInOut.sh

---
+++ scripts/TPYoga_PenInOut.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Deactivate touchscreen when wacom pen is in proximity
+# Activate touchscreen when wacom pen gets out
+
+sleeptime="0.3s"
+lastPenPosition="Proximity=Out"
+while true
+do 
+    mssg="`xinput query-state "Wacom ISDv4 EC Pen stylus" | grep Proximity`"
+    if [ ${#mssg} -gt 1 ]
+    then
+        inout="`echo $mssg | awk '{print $3}'`"
+        case "$inout" in
+            "Proximity=In" )
+                if [ $lastPenPosition != $inout ]
+                then
+                    xinput --disable "ELAN Touchscreen"
+                    lastPenPosition=$inout
+                fi
+                ;;
+            "Proximity=Out" )
+                if [ $lastPenPosition != $inout ]
+                then
+                    xinput --enable "ELAN Touchscreen"
+                    lastPenPosition=$inout
+                fi
+                ;;
+        esac
+    fi
+    sleep $sleeptime
+done
