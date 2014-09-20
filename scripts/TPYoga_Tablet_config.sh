b0b18bbca570da6cd5f8240a4be69f7461e7efc0
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-09-20 17:30:15 -0400

    Changed some modes.

--- scripts/TPYoga_Tablet_config.sh
+++ scripts/TPYoga_Tablet_config.sh
@@ -1,4 +1,4 @@
-sudo cat >/etc/acpi/events/tp_tablet_mode  <<SCRIPT
+cat >/etc/acpi/events/tp_tablet_mode  <<SCRIPT
 event=ibm/hotkey LEN0068:00 00000080 000060c0
 action=/home/bas/git/bas-rustenburg/thinkpad-yoga/scripts/TPYoga_TabletMode.sh
 SCRIPT
