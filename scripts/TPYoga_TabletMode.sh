afe78791be56633d925c693226b273b8fd0babdd
Bas Rustenburg <bas-rustenburg@users.noreply.github.com>
2014-09-20 17:37:00 -0400

    Update TPYoga_TabletMode.sh

--- scripts/TPYoga_TabletMode.sh
+++ scripts/TPYoga_TabletMode.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
+# When converted to tablet, switch off touchpad, rotate screen and launch onboard
+# Since opening and closing tabletmode is signaled by the same ACPI event, the script toggles parameters.
 
-/usr/bin/firefox
+export XAUTHORITY=`ls -1 /home/*/.Xauthority | head -n 1`
+export DISPLAY=":`ls -1 /tmp/.X11-unix/ | sed -e s/^X//g | head -n 1`"
+SCRIPTS=/home/bas/git/bas-rustenburg/thinkpad-yoga/scripts
+
+
+touchpad=$(xinput list-props "SynPS/2 Synaptics TouchPad" | grep "Device Enabled" | awk -F ":" '{print $2}')
+if [ $touchpad -eq 1 ]; then
+	${SCRIPTS}/TPYoga_Rotate.sh up
+	xinput --set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
+	sudo -b -u \#1000 onboard
+else
+	${SCRIPTS}/TPYoga_Rotate.sh down
+	xinput --set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 1
+	killall onboard
+fi
