aa0cdfba27ce1cbdefbe0c355ba28f11c6a01958
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-09-20 17:33:27 -0400

    Try config again.

--- scripts/TPYoga_Tablet_config.sh
+++ scripts/TPYoga_Tablet_config.sh
@@ -1,4 +1,4 @@
 cat >/etc/acpi/events/tp_tablet_mode  <<SCRIPT
 event=ibm/hotkey LEN0068:00 00000080 000060c0
-action=/home/bas/git/bas-rustenburg/thinkpad-yoga/scripts/TPYoga_TabletMode.sh
+action=bash /home/bas/git/bas-rustenburg/thinkpad-yoga/scripts/TPYoga_TabletMode.sh
 SCRIPT
