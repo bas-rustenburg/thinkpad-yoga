5985ec1f73dd01ca08b3bff18fcd506b327b60c8
Bas Rustenburg <bas-rustenburg@users.noreply.github.com>
2014-09-23 21:30:59 -0400

    Update and rename TPYoga_Tablet_config.sh to TPYoga_Tablet_acpi_config.sh
    Run as root

--- scripts/TPYoga_Tablet_acpi_config.sh
+++ scripts/TPYoga_Tablet_acpi_config.sh
@@ -2,3 +2,5 @@ cat >/etc/acpi/events/tp_tablet_mode  <<SCRIPT
 event=ibm/hotkey LEN0068:00 00000080 000060c0
 action=bash /home/bas/Git/bas-rustenburg/thinkpad-yoga/scripts/TPYoga_TabletMode.sh
 SCRIPT
+
+sudo restart acpid
